5522f49875ec33f6291e8204858977fcb62cd660
lpelczar <lukepelczar@gmail.com>
2018-07-01 21:45:32 +0200

    Add sending response helper method and fix annotation

--- src/main/java/com/codecool/webroute/Controller.java
+++ src/main/java/com/codecool/webroute/Controller.java
@@ -3,12 +3,25 @@ package com.codecool.webroute;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
+import java.io.IOException;
+import java.io.OutputStream;
+
 public class Controller implements HttpHandler {
 
     public void handle(HttpExchange httpExchange) {
 
         String path = httpExchange.getRequestURI().getPath();
-        System.out.println(path);
 
+    }
+
+    @WebRoute("/test")
+    private void onTest(HttpExchange httpExchange) throws IOException {
+    }
+
+    private void sendResponse(HttpExchange httpExchange, String response) throws IOException {
+        httpExchange.sendResponseHeaders(200, response.length());
+        OutputStream os = httpExchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
     }
 }
--- src/main/java/com/codecool/webroute/WebServer.java
+++ src/main/java/com/codecool/webroute/WebServer.java
@@ -5,9 +5,9 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 
-public class WebServer {
+class WebServer {
 
-    public void start() throws IOException {
+    void start() throws IOException {
 
         HttpServer server = HttpServer.create(new InetSocketAddress(9000), 0);
         server.createContext("/", new Controller());
